282378aea95ec7414281e61ba1199b9cee83399f
Adam Graham <adam.zigurous@gmail.com>
2020-10-28 21:58:31 -0500

    Bundle all css files into the library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
-import React from 'react'
-import styles from './styles.module.css'
+import './base/root.css';
+import './base/index.css';
+import './base/themes.css';
+import './base/typography.css';
+import './base/colors.css';
+import './base/scrollbar.css';
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+import './objects/display.css';
+import './objects/box-alignment.css';
+import './objects/overflow.css';
+import './objects/flex.css';
+import './objects/widths.css';
+import './objects/heights.css';
+import './objects/margins.css';
+import './objects/padding.css';
+
+import './layout/container.css';
+import './layout/cover.css';
+import './layout/bleed.css';
+import './layout/parallax.css';
+
+import './components/app-footer.css';
+import './components/app-header.css';
+import './components/button-group.css';
+import './components/button.css';
+import './components/divider.css';
+import './components/embedded-video.css';
+import './components/logo.css';
+import './components/nav-bar.css';
+import './components/nav-menu.css';
+import './components/page-banner.css';
+import './components/social-button.css';
+import './components/social-icon.css';
+import './components/social-nav-links.css';
+import './components/thumbnail.css';
