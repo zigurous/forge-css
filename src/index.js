01f4a7d01c8235048f86d866772c7994bf074d6d
Adam Graham <adam@zigurous.com>
2020-12-31 06:59:22 -0600

    Import object classes after layout classes to allow overrides

--- src/index.js
+++ src/index.js
@@ -12,6 +12,15 @@ import './elements/button.css';
 import './elements/hr.css';
 import './elements/ul.css';
 
+import './layout/grid.css';
+import './layout/container.css';
+import './layout/page.css';
+import './layout/cover.css';
+import './layout/split-cover.css';
+import './layout/title-card.css';
+import './layout/bleed.css';
+import './layout/parallax.css';
+
 import './objects/box-alignment.css';
 import './objects/box-shadow.css';
 import './objects/cursor.css';
@@ -24,15 +33,6 @@ import './objects/overflow.css';
 import './objects/padding.css';
 import './objects/typography.css';
 import './objects/widths.css';
-
-import './layout/grid.css';
-import './layout/container.css';
-import './layout/page.css';
-import './layout/cover.css';
-import './layout/split-cover.css';
-import './layout/title-card.css';
-import './layout/bleed.css';
-import './layout/parallax.css';
 
 import './themes/light.css';
 import './themes/dark.css';
