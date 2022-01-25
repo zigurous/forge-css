c3600b94f199f049b37d51dc318ba9ac893462e4
Adam Graham <adam@zigurous.com>
2022-01-25 11:19:19 -0600

    Update example src code

--- example/src/index.js
+++ example/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import '@zigurous/styles/dist/index.css';
+import '@zigurous/css-styles/dist/index.css';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
