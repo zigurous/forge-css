af3e2e7d97b7aea16ce37faf37a1ead1db608687
Adam Graham <adam@zigurous.com>
2024-11-16 18:31:31 -0600

    Delete old example directory

--- example/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-import '@zigurous/css-styles/dist/index.css';
-import './index.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
